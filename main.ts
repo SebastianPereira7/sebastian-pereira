567cd718421cb539d398aaca6b38a0ff166141c6
SebastianPereira7 <91557444+SebastianPereira7@users.noreply.github.com>
2021-09-28 11:23:25 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,11 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.clearScreen()
+})
